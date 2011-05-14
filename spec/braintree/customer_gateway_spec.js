123200af5f9f7cfa6fc455bc5539f62598dc410d
dan <code@getbraintree.com>
2011-05-13 19:26:58 -0700

    customer update

--- spec/braintree/customer_gateway_spec.js
+++ spec/braintree/customer_gateway_spec.js
@@ -123,4 +123,77 @@ vows.describe('CustomerGateway').addBatch({
       }
     },
   },
+
+  'update': {
+    'for a minimal case': {
+      topic: function () {
+        var callback = this.callback;
+        specHelper.defaultGateway.customer.create(
+          {
+            firstName: 'Old First Name',
+            lastName: 'Old Last Name'
+          },
+          function (err, response) {
+            specHelper.defaultGateway.customer.update(
+              response.customer.id,
+              {
+                firstName: 'New First Name',
+                lastName: 'New Last Name'
+              },
+              callback
+            )
+          }
+        );
+      },
+      'does not have an error': function (err, response) { assert.isNull(err); },
+      'is succesful': function (err, response) { assert.equal(response.success, true); },
+      'has updated customer attributes': function (err, response) {
+        assert.equal(response.customer.firstName, 'New First Name');
+        assert.equal(response.customer.lastName, 'New Last Name');
+      }
+    },
+
+    'when not found': {
+      topic: function () {
+        specHelper.defaultGateway.customer.update('nonexistent_customer', {}, this.callback);
+      },
+      'returns a not found error': function (err, response) {
+        assert.equal(err.type, braintree.errorTypes.notFoundError);
+      }
+    },
+
+    'with errors': {
+      topic: function () {
+        var callback = this.callback;
+        specHelper.defaultGateway.customer.create(
+          {},
+          function (err, response) {
+            specHelper.defaultGateway.customer.update(
+              response.customer.id,
+              {
+                email: 'invalid_email_address'
+              },
+              callback
+            )
+          }
+        );
+      },
+      'is unsuccessful': function (err, response) { assert.equal(response.success, false); },
+      'has a unified message': function (err, response) {
+        assert.equal(response.message, 'Email is an invalid format.');
+      },
+      'has an error on email': function (err, response) {
+        assert.equal(
+          response.errors.for('customer').on('email').code,
+          '81604'
+        );
+      },
+      'has an attribute on ValidationError objects': function (err, response) {
+        assert.equal(
+          response.errors.for('customer').on('email').attribute,
+          'email'
+        );
+      }
+    }
+  },
 }).export(module);
