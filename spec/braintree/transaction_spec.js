3d855433b84aa443dd2a63abed5b7f116be8decc
dan <code@getbraintree.com>
2011-05-13 17:50:14 -0700

    credit transactions

--- spec/braintree/transaction_spec.js
+++ spec/braintree/transaction_spec.js
@@ -3,7 +3,66 @@ require('../spec_helper');
 var _ = require('underscore')._;
 
 vows.describe('Transaction').addBatch({
-  'create': {
+  'credit': {
+    'for a minimal case': {
+      topic: function () {
+        specHelper.defaultGateway.transaction.credit({
+          amount: '5.00',
+          creditCard: {
+            number: '5105105105105100',
+            expirationDate: '05/12'
+          }
+        }, this.callback);
+      },
+      'does not have an error': function (err, response) { assert.isNull(err); },
+      'is succesful': function (err, response) { assert.equal(response.success, true); },
+      'is a credit': function (err, response) { assert.equal(response.transaction.type, 'credit'); },
+      'is for 5.00': function (err, response) { assert.equal(response.transaction.amount, '5.00'); },
+      'has a masked number of 510510******5100': function (err, response) {
+        assert.equal(response.transaction.creditCard.maskedNumber, '510510******5100');
+      }
+    },
+
+    'with errors': {
+      topic: function () {
+        specHelper.defaultGateway.transaction.credit({
+          creditCard: {
+            number: '5105105105105100'
+          }
+        }, this.callback);
+      },
+      'is unsuccessful': function (err, response) { assert.equal(response.success, false); },
+      'has a unified message': function (err, response) {
+        assert.equal(response.message, 'Amount is required.\nExpiration date is required.');
+      },
+      'has an error on amount': function (err, response) {
+        assert.equal(
+          response.errors.for('transaction').on('amount').code,
+          '81502'
+        );
+      },
+      'has an attribute on ValidationError objects': function (err, response) {
+        assert.equal(
+          response.errors.for('transaction').on('amount').attribute,
+          'amount'
+        );
+      },
+      'has a nested error on creditCard.expirationDate': function (err, response) {
+        assert.equal(
+          response.errors.for('transaction').for('creditCard').on('expirationDate').code,
+          '81709'
+        );
+      },
+      'returns deepErrors': function (err, response) {
+        var errorCodes = _.map(response.errors.deepErrors(), function (error) { return error.code; });
+        assert.equal(2, errorCodes.length);
+        assert.include(errorCodes, '81502');
+        assert.include(errorCodes, '81709');
+      }
+    }
+  },
+
+  'sale': {
     'for a minimal case': {
       topic: function () {
         specHelper.defaultGateway.transaction.sale({
