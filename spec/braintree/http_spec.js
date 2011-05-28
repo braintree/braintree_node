8ed1e9b933090865f2fcaa8bb46c3b055927440a
dan <code@getbraintree.com>
2011-05-28 15:05:59 -0500

    return a server error on 500 responses

--- spec/braintree/http_spec.js
+++ spec/braintree/http_spec.js
@@ -3,13 +3,15 @@ var Config = require('../../lib/braintree/config').Config;
 var Http = require('../../lib/braintree/http').Http;
 
 vows.describe('Http').addBatch({
-  'UnexpectedError': {
-    topic: function () {
-      var http = Http(Config(specHelper.defaultConfig));
-      http.post('/test/error', '', this.callback);
-    },
-    'returns the UnexpectedError': function (err, response) {
-      assert.equal(err.type, braintree.errorTypes.unexpectedError);
+  'request': {
+    'when the http response status is 500': {
+      topic: function () {
+        var http = Http(Config(specHelper.defaultConfig));
+        http.post('/test/error', '', this.callback);
+      },
+      'returns a ServerError': function (err, response) {
+        assert.equal(err.type, braintree.errorTypes.serverError);
+      }
     }
   }
 }).export(module);
