674e736fd62fba1acf8b48a3d235af5059692f38
evanhahn and jonchew <code@getbraintree.com>
2017-03-14 20:46:26 +0000

    BTDXCLTS-391 - Add IdealPayment.find

--- spec/integration/braintree/ideal_payment_gateway_spec.js
+++ spec/integration/braintree/ideal_payment_gateway_spec.js
@@ -3,6 +3,34 @@
 let Transaction = require('../../../lib/braintree/transaction').Transaction;
 
 describe('IdealPaymentGateway', function () {
+  describe('find', function () {
+    it('finds the Ideal payment', done => {
+      specHelper.generateValidIdealPaymentNonce(function (nonce) {
+
+        specHelper.defaultGateway.idealPayment.find(nonce, function (err, idealPayment) {
+          assert.isNull(err);
+
+          assert.match(idealPayment.id, /^idealpayment_\w{6,}$/);
+          assert.match(idealPayment.idealTransactionId, /^\d{16,}$/);
+          assert.isDefined(idealPayment.currency);
+          assert.isDefined(idealPayment.amount);
+          assert.isDefined(idealPayment.status);
+          assert.isDefined(idealPayment.orderId);
+          assert.isDefined(idealPayment.issuer);
+          assert(idealPayment.approvalUrl.startsWith('https://'));;
+          assert.isDefined(idealPayment.ibanBankAccount.accountHolderName);
+          assert.isDefined(idealPayment.ibanBankAccount.bic);
+          assert.isDefined(idealPayment.ibanBankAccount.maskedIban);
+          assert.match(idealPayment.ibanBankAccount.ibanAccountNumberLast4, /^\d{4}$/);
+          assert.isDefined(idealPayment.ibanBankAccount.ibanCountry);
+          assert.isDefined(idealPayment.ibanBankAccount.description);
+
+          done();
+        });
+      });
+    });
+  });
+
   describe('sale', () => {
     it('transacts on an Ideal payment nonce', done => {
       specHelper.generateValidIdealPaymentNonce(function (nonce) {
@@ -18,8 +46,8 @@ describe('IdealPaymentGateway', function () {
           assert.match(response.transaction.idealPaymentDetails.idealPaymentId, /^idealpayment_\w{6,}$/);
           assert.match(response.transaction.idealPaymentDetails.idealTransactionId, /^\d{16,}$/);
           assert.isTrue(response.transaction.idealPaymentDetails.imageUrl.startsWith('https://'));
-          assert.isNotNull(response.transaction.idealPaymentDetails.maskedIban);
-          assert.isNotNull(response.transaction.idealPaymentDetails.bic);
+          assert.isDefined(response.transaction.idealPaymentDetails.maskedIban);
+          assert.isDefined(response.transaction.idealPaymentDetails.bic);
 
           done();
         });
