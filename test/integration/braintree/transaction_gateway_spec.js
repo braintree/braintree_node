908caca23e48419d43a81dc32674217d18fe2efd
Jorge Munoz <jorge.munoz@getbraintree.com>
2021-07-12 19:33:31 +0000

    Remove lingering tax asserts

--- test/integration/braintree/transaction_gateway_spec.js
+++ test/integration/braintree/transaction_gateway_spec.js
@@ -5072,8 +5072,6 @@ describe('TransactionGateway', function () {
         assert.isString(transaction.paypalAccount.sellerProtectionStatus);
         assert.isString(transaction.paypalAccount.captureId);
         assert.isString(transaction.paypalAccount.refundId);
-        assert.isString(transaction.paypalAccount.taxId);
-        assert.isString(transaction.paypalAccount.taxIdType);
         assert.isString(transaction.paypalAccount.transactionFeeAmount);
         assert.isString(transaction.paypalAccount.transactionFeeCurrencyIsoCode);
         assert.isString(transaction.paypalAccount.refundFromTransactionFeeAmount);
