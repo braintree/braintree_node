8c9093df1754db2cb7068cc022ae9d50845d3f0e
Chenhong Shi <chenshi@paypal.com>
2022-04-12 14:15:39 -0700

    fix ut

--- test/unit/braintree/exchange_rate_quote_gateway_spec.js
+++ test/unit/braintree/exchange_rate_quote_gateway_spec.js
@@ -1,5 +1,8 @@
 "use strict";
 
+const sinon = require("sinon");
+let GraphQLClient =
+  require("../../../lib/braintree/graphql_client").GraphQLClient;
 let ExchangeRateQuoteGateway =
   require("../../../lib/braintree/exchange_rate_quote_gateway").ExchangeRateQuoteGateway;
 
@@ -45,6 +48,56 @@ describe("ExchangeRateQuoteGateway", () =>
             "These keys are invalid: quotes[invalidKey]"
           );
           assert.notExists(params);
+          done();
+        }
+      );
+    });
+
+    it("accepts valid keys", function (done) {
+      sinon.stub(GraphQLClient.prototype, "query").resolves({
+        data: {
+          generateExchangeRateQuote: {
+            clientMutationId: null,
+            quotes: [
+              {
+                id: "ZXhjaGFuZ2VyYXRlcXVvdGVfMDEyM0FCQw",
+                baseAmount: {
+                  value: "15.16",
+                  currencyCode: "USD",
+                },
+                quoteAmount: {
+                  value: "12.16",
+                  currencyCode: "EUR",
+                },
+                exchangeRate: "0.997316360864",
+                tradeRate: "0.01",
+                expiresAt: "2021-06-16T02:00:00.000000Z",
+                refreshesAt: "2021-06-16T00:00:00.000000Z",
+              },
+            ],
+          },
+        },
+        extensions: {
+          requestId: "9e30486e-be31-4477-942f-26f30e93a5ac",
+        },
+      });
+
+      let exchangeRateQuoteGateway = new ExchangeRateQuoteGateway(fakeGateway);
+      let exchangeRateQuoteRequest = {
+        quotes: [
+          {
+            baseCurrency: "USD",
+            quoteCurrency: "EUR",
+            baseAmount: "15.16",
+          },
+        ],
+      };
+
+      exchangeRateQuoteGateway.generate(
+        exchangeRateQuoteRequest,
+        (err, params) => {
+          assert.notExists(err);
+          assert.isTrue(params.success);
           done();
         }
       );
